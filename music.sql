f10f67226b77d64639b49a12afca69b88293c9fe
TheOneChris <60333802+TheOneChris@users.noreply.github.com>
2022-04-10 12:18:43 -0400

    finished

--- music.sql
+++ music.sql
@@ -1,1 +1,17 @@
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY,
+    artist_name TEXT);
 
+
+CREATE TABLE albums (
+	id INTEGER PRIMARY KEY,
+	album_title TEXT,
+	artist_name TEXT);
+
+
+CREATE TABLE songs (
+	id INTEGER PRIMARY KEY,
+	song_title TEXT,
+	album_title TEXT,
+	track_no INTEGER,
+	length INTEGER);
